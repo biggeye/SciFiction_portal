150b49ca9ab332d3b7a9bbfa3f0de9ccab20da2c
biggEye <96358030+biggeye@users.noreply.github.com>
2023-06-05 13:56:42 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: '/api/xilabs/:path*',
-          destination:
-            process.env.NODE_ENV === 'development'
-              ? 'http://127.0.0.1:5000/api/xilabs/:path*'
-              : '/api/xilabs/',
-        },
-      ]
-    },
-  }
-  
+
   module.exports = nextConfig

54e6735c2b65d28cdbfe1e4e20854c6e23348feb
biggEye <96358030+biggeye@users.noreply.github.com>
2023-06-05 13:58:15 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-  module.exports = nextConfig
+  nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "pckovrpqrkyqinifydcs.supabase.co",
+      }
+    ],
+  },
+};
+// next.config.js
+module.exports = {
+  serverRuntimeConfig: {
+    // Increase max body size to 10MB (default is 1MB)
+    maxBodySize: '4mb',
+  },
+};

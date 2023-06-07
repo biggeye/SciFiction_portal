7726b889ef668fbb199bf9fd6cda649a6a85be28
biggeye <justin_bonner@protonmail.com>
2023-06-07 13:44:03 -0700

    s

--- components/creators/shared/VideoPlayer.jsx
+++ components/creators/shared/VideoPlayer.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+
 
 const VideoPlayer = ({ s3Url }) => {
   const videoRef = useRef(null);

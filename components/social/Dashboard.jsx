b1a356d48aff8a5faa7ddfe17014481b36a32010
biggEye <96358030+biggeye@users.noreply.github.com>
2023-10-16 13:52:51 -0700

    .

--- components/social/Dashboard.jsx
+++ components/social/Dashboard.jsx
@@ -1,9 +1,33 @@
 import React, { useEffect, useState } from "react";
 import TikAPIConnectButton from "../auth/TikTok/TikAPI";
-import { checkAccessToken } from "./checkAccessToken"; // Assuming you have exported the checkAccessToken function
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 const supabase = useSupabaseClient();
 const user = useUser();
+const checkAccessToken = async (user) => {
+  try {
+    const { data, error } = await supabase
+      .from("oauth2_tokens")
+      .select("access_token")
+      .eq("user_id", user)
+      .eq("provider", "TikTok");
+
+    if (error) {
+      console.error(error);
+      return null;
+    }
+
+    if (data && data.length > 0) {
+      // Access token exists
+      return data[0].access_token;
+    } else {
+      // Access token does not exist
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 const Dashboard = () => {
   const [accessToken, setAccessToken] = useState(null);
   

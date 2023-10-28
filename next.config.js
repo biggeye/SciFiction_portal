48bc9ecaec5a004a5676ff1c8e44a2dd1846ff15
biggeye <justin_bonner@protonmail.com>
2023-10-28 13:43:19 -0700

    .

--- next.config.js
+++ next.config.js
@@ -14,13 +14,19 @@
       },
       {
         protocol: "https",
-        hostname: "pckovrpqrkyqinifydcs.supabase.co",
+        hostname: "xqdkoozsrecjixhnpoou.supabase.co",
       }
     ],
   },
 };
 // next.config.js
 module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the alias for the root directory
+    config.resolve.alias['@'] = path.join(__dirname, '/');
+
+    return config;
+  },
   serverRuntimeConfig: {
     // Increase max body size to 10MB (default is 1MB)
     maxBodySize: '4mb',

188f01d965de0c96dfacd98e6fbcb4264d16ca29
biggeye <justin_bonner@protonmail.com>
2023-10-28 13:39:11 -0700

    .

--- data/redux/store.js
+++ data/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import galleryReducer from './gallerySlice';
+import galleryReducer from '../../utils/redux/gallerySlice';
 
 export const store = configureStore({
   reducer: {
--- utils/replicate/toBlob.js
+++ utils/replicate/toBlob.js
@@ -2,6 +2,7 @@ const toBlob = async (fileUrl) => {
 const response = await fetch(fileUrl);
 const imgBlob = await response.blob();
 
+
 return imgBlob;
 };
 export default toBlob;

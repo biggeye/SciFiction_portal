d6dac837c3dd4138b74308a5aae5ad80dfddaa97
biggEye <96358030+biggeye@users.noreply.github.com>
2023-11-23 14:41:35 -0800

    Update Chat.jsx

--- components/creators/openai/Chat.jsx
+++ components/creators/openai/Chat.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import { Grid, GridItem, Button, useDisclosure, IconButton, VStack, Input, Collapse, Box, Heading, Text } from '@chakra-ui/react';
+import {
+  ChakraProvider, Flex, Box, VStack, Input, Button, IconButton, Collapse, Heading, Text, 
+  Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, 
+  FormControl, InputGroup, useDisclosure, useBreakpointValue, useColorModeValue
+} from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { MdImage, MdAudiotrack, MdTextFields, MdOndemandVideo } from 'react-icons/md';
+
+// Assuming useChat is a custom hook you've created
+import useChat from './useChat'; 
 
 const ToolIcons = {
   Image: MdImage,
@@ -12,7 +19,7 @@ const ToolIcons = {
 
 export default function Chat() {
   const [selectedTool, setSelectedTool] = useState(null);
-  const { isOpen, onOpen, onToggle, onClose } = useDisclosure();
+  const { isOpen, onToggle, onClose } = useDisclosure();
   const { messages, handleSubmit, input, handleInputChange } = useChat();
   const isDrawer = useBreakpointValue({ base: true, md: false });
 
@@ -38,19 +45,23 @@ export default function Chat() {
         return <Text>Select a tool from the toolbox.</Text>;
     }
   };
+
   return (
     <ChakraProvider>
       <Flex direction={{ base: 'column', md: 'row' }}>
-        {isDrawer ? (
-          <IconButton
-            icon={<HamburgerIcon />}
-            onClick={onOpen}
-            variant="outline"
-            aria-label="Open Menu"
-          />
-        ) : (
-          <Text>History</Text>
-        )}
+        <Box>
+          {isDrawer ? (
+            <IconButton
+              icon={<HamburgerIcon />}
+              onClick={onToggle}
+              variant="outline"
+              aria-label="Open Menu"
+            />
+          ) : (
+            <Text>History</Text>
+          )}
+        </Box>
+
         <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
           <DrawerOverlay />
           <DrawerContent>
@@ -60,57 +71,59 @@ export default function Chat() {
           </DrawerContent>
         </Drawer>
 
-        <Container maxW="container.xl">
-          <Flex justify="space-between" wrap="wrap">
-            <Box>
-              {Object.entries(ToolIcons).map(([tool, Icon]) => (
-                <IconButton
-                  key={tool}
-                  aria-label={tool}
-                  icon={<Icon />}
-                  onClick={() => handleToolSelection(tool)}
-                  isRound
-                  m={2}
-                />
+        <Box flex="1" maxW="container.xl" p={4}>
+          <VStack align="stretch" spacing={4}>
+            <Flex justify="space-between" wrap="wrap">
+              <Box>
+                {Object.entries(ToolIcons).map(([tool, Icon]) => (
+                  <IconButton
+                    key={tool}
+                    aria-label={tool}
+                    icon={<Icon />}
+                    onClick={() => handleToolSelection(tool)}
+                    isRound
+                    m={2}
+                  />
+                ))}
+              </Box>
+              <VStack spacing={4}>
+                <Heading>{selectedTool || 'Output'}</Heading>
+                <Collapse in={isOpen} animateOpacity>
+                  <Box p={4} border="1px" borderColor="gray.200" rounded="md">
+                    {renderInputForm()}
+                  </Box>
+                </Collapse>
+              </VStack>
+            </Flex>
+          </VStack>
+
+          <VStack spacing={{ base: 2, sm: 4, md: 6 }} align="stretch">
+            <Box bg={bg} w={{ base: '100%', md: '80%', lg: '60%' }} p={4} color="black">
+              <form onSubmit={handleSubmit}>
+                <FormControl>
+                  <InputGroup>
+                    <Input
+                      name="prompt"
+                      value={input}
+                      onChange={handleInputChange}
+                      id="input"
+                      placeholder="Type your message..."
+                    />
+                    <Button colorScheme="blue" type="submit">
+                      Submit
+                    </Button>
+                  </InputGroup>
+                </FormControl>
+              </form>
+              {messages.map((m) => (
+                <Box key={m.id} bg={responseBg} p={3} my={2} rounded="md">
+                  {m.role === 'user' ? 'User: ' : 'AI: '}
+                  {m.content}
+                </Box>
               ))}
             </Box>
-            <VStack spacing={4} flex="1" p={4}>
-              <Heading>{selectedTool || 'Output'}</Heading>
-              <Collapse in={isOpen} animateOpacity>
-                <VStack spacing={4} p={4} border="1px" borderColor="gray.200" rounded="md">
-                  {renderInputForm()}
-                </VStack>
-              </Collapse>
-            </VStack>
-          </Flex>
-        </Container>
-        <VStack flex="1" spacing={{ base: 2, sm: 4, md: 6 }} align="stretch">
-          <Box bg={bg} w={{ base: '100%', md: '80%', lg: '60%' }} p={4} color="black">
-            <form onSubmit={handleSubmit}>
-              <FormControl>
-                <InputGroup>
-                  <Input
-                    mt={{ base: 2, md: 4 }}
-                    name="prompt"
-                    value={input}
-                    onChange={handleInputChange}
-                                        id="input"
-                    placeholder="Type your message..."
-                  />
-                  <Button mt={{ base: 2, md: 4 }} colorScheme="blue" type="submit">
-                    Submit
-                  </Button>
-                </InputGroup>
-              </FormControl>
-            </form>
-            {messages.map((m) => (
-              <Box key={m.id} className={card}>
-                {m.role === 'user' ? 'User: ' : 'AI: '}
-                {m.content}
-              </Box>
-            ))}
-          </Box>
-        </VStack>
+          </VStack>
+        </Box>
       </Flex>
     </ChakraProvider>
   );

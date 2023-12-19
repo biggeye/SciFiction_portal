e33858df6ef32d6c0efebc3e63c3f572f9d592c3
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-19 15:58:45 -0800

    Update AuthForm.jsx

--- components/auth/AuthForm.jsx
+++ components/auth/AuthForm.jsx
@@ -7,6 +7,26 @@ export default function AuthForm() {
   const supabase = createClientComponentClient()
 
   return (
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Log-In</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Auth
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              theme="dark"
+              providers={["google", "github"]}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     <Auth
       supabaseClient={supabase}
       view="magic_link"
@@ -14,7 +34,7 @@ export default function AuthForm() {
       theme="light"
       showLinks={false}
       providers={['google']}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo="http://localhost:3000/api/auth/callback"
     />
   )
 }

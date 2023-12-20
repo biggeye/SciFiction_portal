ae36db5b0e0ab2d261c60bb65b98fe78e1765e52
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-19 16:08:31 -0800

    Update layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import { GeistSans } from 'geist/font/sans'
+import './globals.css'
+import Navbar from '@/components/Navbar';
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'SciLive (alpha)',
+  description: 'powered by SciFiction.com',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+          <body className="body bg-slate-100">
+          <Navbar />
+          <main className="bg-gradient-to-b from-slate-100 to-gray-300">
+              {children}
+            </main>
+          </body>
+  
+    </html>
+  )
+}
